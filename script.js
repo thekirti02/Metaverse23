cf9b42c0774fac886ec0bf2cce77cf7a73c3145e
Kirti <105008820+thekirti02@users.noreply.github.com>
2023-08-15 14:28:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -44,9 +44,35 @@ AFRAME.registerComponent('camera-controls', {
   });
   
 
+  const numberOfCoins = 1000;
+  for (let i = 0; i < numberOfCoins; i++) {
+    const coin = document.createElement('a-sphere');
+    const x = Math.random() * 200 - 100;
+    const y = 23 
+    const z = Math.random() * 200 - 100;
+    coin.setAttribute('position', `${x} ${y} ${z}`);
+    coin.setAttribute('radius', '0.1');
+    coin.setAttribute('color', 'gold');
+    coin.setAttribute('class', 'coin');
+    coin.setAttribute('dynamic-body', ''); // Add dynamic-body for physic 
+    document.querySelector('a-scene').appendChild(coin);
+  }
+// Collision detection and removal logic
+const character = document.querySelector('#character');
+const coins = document.querySelectorAll('.coin');
+const counter = document.querySelector('#counter');
 
+// A-Frame physics collision event
+character.addEventListener('collide', function(event) {
+  const collidedElement = event.detail.body.el;
 
-
+  if (collidedElement.classList.contains('coin')) {
+    collidedElement.parentNode.removeChild(collidedElement);
+    collisionCount++;
+    counter.setAttribute('text', { value: collisionCount });
+    collisionLabel.setAttribute('value', `Collisions: ${collisionCount}`);
+  }
+});
 
 
 
